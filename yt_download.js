db0d45da08011b879bff251debf5c73d76d39694
MakazhuBR <makazbr0@sewanee.edu>
2022-10-22 14:06:31 -0500

    add ytdl code

--- yt_download.js
+++ yt_download.js
@@ -1,2 +1,12 @@
-console.log("hey");
-// TODO: put the ytdl code here
+/*
+  Downloads an MP4 Youtube video
+*/
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+    // TODO: change the URL to the video URL provided by the tab
+    //       using tabs[0].url
+    ytdl('https://www.youtube.com/watch?v=ZU0f8_C5Pm0')
+        .pipe(fs.createWriteStream('video.mp4'));
+});
